6c9747a2a6669e30f22ba9525f3d0d3ec58e07f6
Sam <admin@gatewayweb.net>
2023-01-16 09:43:46 -0500

    initial commit

---
+++ src/buildClassString.ts
@@ -0,0 +1,70 @@
+import { twMerge } from "tailwind-merge";
+
+export interface StringProps {
+  [key: string]: any;
+}
+
+export interface VariantProps {
+  props: StringProps;
+  classNames: StringProps;
+}
+
+export const buildClassString = (
+  variants: VariantProps[],
+  props: { [key: string]: any }
+) => {
+  let classes: { [key: string]: any } = {};
+  let propsObject: { [key: string]: any } = {};
+  let variantProps: Array<string> = [];
+
+  variants?.forEach((variant) => {
+    let availableProps = Object.keys(variant?.props);
+
+    availableProps
+      .filter((prop) => !variantProps.includes(prop))
+      .forEach((prop) => {
+        variantProps = [...variantProps, prop];
+      });
+  });
+
+  const componentProps = Object.keys(props)
+    .filter((key) => variantProps.includes(key))
+    .reduce((cur, key) => {
+      return Object.assign(cur, { [key]: props[key] });
+    }, propsObject);
+
+  variants?.forEach((variant) => {
+    const vProps = Object.keys(variant?.props);
+    const requiredProps = vProps.filter((prop) => variantProps.includes(prop));
+
+    if (requiredProps?.length) {
+      const someNoMatch = requiredProps?.some((requiredProp) => {
+        return (
+          componentProps?.[requiredProp] !== variant?.props?.[requiredProp]
+        );
+      });
+
+      if (!someNoMatch || variant?.props?.name === "default") {
+        Object.keys(variant?.classNames)?.forEach((classKey) => {
+          const existingClassNames = classes?.[classKey]
+            ? classes[classKey]
+            : "";
+          classes = {
+            ...classes,
+            [classKey]: twMerge(
+              `${existingClassNames} ${variant?.classNames?.[classKey]}${
+                classKey === "root"
+                  ? `${props?.overrides ? ` ${props.overrides}` : ""}`
+                  : ``
+              }`
+            ),
+          };
+        });
+      }
+    }
+  });
+
+  return classes;
+};
+
+export default buildClassString;
